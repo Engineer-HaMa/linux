3675e22c08952e9bbef56a41a025a5f3b6ed2c5f
Andreas Hindborg <a.hindborg@samsung.com>
2023-09-06 08:58:43 +0200

    rust: update blk-mq for nvme

--- rust/kernel/block/mq/gen_disk.rs
+++ rust/kernel/block/mq/gen_disk.rs
@@ -28,7 +28,7 @@ pub struct GenDiskBuilder<T> {
     physical_block_size: u32,
     capacity_sectors: u64,
     max_hw_discard_sectors: u32,
-    #[cfg(CONFIG_BLK_DEV_ZONED)]
+#[cfg(CONFIG_BLK_DEV_ZONED)]
     zoned: bool,
     #[cfg(CONFIG_BLK_DEV_ZONED)]
     zone_size_sectors: u32,
@@ -38,6 +38,8 @@ pub struct GenDiskBuilder<T> {
     forced_unit_access: bool,
     max_sectors: u32,
     virt_boundary_mask: usize,
+    max_segments: u16,
+    max_hw_sectors: u32,
     _p: PhantomData<T>,
 }
 
@@ -49,7 +51,7 @@ impl<T> Default for GenDiskBuilder<T> {
             physical_block_size: bindings::PAGE_SIZE as u32,
             capacity_sectors: 0,
             max_hw_discard_sectors: 0,
-            #[cfg(CONFIG_BLK_DEV_ZONED)]
+#[cfg(CONFIG_BLK_DEV_ZONED)]
             zoned: false,
             #[cfg(CONFIG_BLK_DEV_ZONED)]
             zone_size_sectors: 0,
@@ -59,6 +61,8 @@ impl<T> Default for GenDiskBuilder<T> {
             forced_unit_access: false,
             max_sectors: 0,
             virt_boundary_mask: 0,
+            max_segments: 0,
+            max_hw_sectors: 0,
             _p: PhantomData,
         }
     }
@@ -130,7 +134,7 @@ impl<T: Operations> GenDiskBuilder<T> {
         self
     }
 
-    /// Mark this device as a zoned block device.
+/// Mark this device as a zoned block device.
     #[cfg(CONFIG_BLK_DEV_ZONED)]
     pub fn zoned(mut self, enable: bool) -> Self {
         self.zoned = enable;
@@ -178,6 +182,18 @@ impl<T: Operations> GenDiskBuilder<T> {
         self
     }
 
+    /// Set the maximum number of segments for this block device.
+    pub fn max_segments(mut self, segments: u16) -> Self {
+        self.max_segments = segments;
+        self
+    }
+
+    /// Set the maximum hardware sectors for this block device.
+    pub fn max_hw_sectors(mut self, sectors: u32) -> Self {
+        self.max_hw_sectors = sectors;
+        self
+    }
+
     /// Build a new `GenDisk` and add it to the VFS.
     pub fn build(
         self,
@@ -196,7 +212,9 @@ impl<T: Operations> GenDiskBuilder<T> {
         lim.logical_block_size = self.logical_block_size;
         lim.physical_block_size = self.physical_block_size;
         lim.max_hw_discard_sectors = self.max_hw_discard_sectors;
-        lim.max_sectors = self.max_sectors;
+lim.max_sectors = self.max_sectors;
+        lim.max_hw_sectors = self.max_hw_sectors;
+        lim.max_segments = self.max_segments;
         lim.virt_boundary_mask = self.virt_boundary_mask;
         if self.rotational {
             lim.features = Feature::Rotational.into();
