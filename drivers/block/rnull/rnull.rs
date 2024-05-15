62dd9b29d77c78bdb2a8c69328e6a81fc9aed55c
Andreas Hindborg <a.hindborg@samsung.com>
2024-05-15 13:44:11 -0600

    block: rust: add `TagSet` private data support
    C block device drivers can attach private data to a `struct
    blk_mq_tag_set`. Add support for this feature for Rust block device
    drivers via the `Operations::TagSetData` associated type.
    
    The private data is passed to `TagSet::new` and is stored in the
    `driver_data` field of the underlying `struct blk_mq_tag_set`. It is
    released when the `TagSet` is dropped.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -249,7 +249,7 @@ impl NullBlkDevice {
         }
 
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, home_node, flags),
+            TagSet::new(submit_queues, (), 256, 1, home_node, flags),
             GFP_KERNEL,
         )?;
 
@@ -525,6 +525,7 @@ where
 impl Operations for NullBlkDevice {
     type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
+    type TagSetData = ();
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
         pin_init!(Pdu {
