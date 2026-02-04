dd91ef6d37c32b41a9b08ceca16db0096724a2f5
Oliver Mangold <oliver.mangold@pm.me>
2026-02-04 12:56:46 +0100

    LIST: [PATCH v14 2/9] rust: rename `AlwaysRefCounted` to `RefCounted`.
    There are types where it may both be reference counted in some cases and
    owned in others. In such cases, obtaining `ARef<T>` from `&T` would be
    unsound as it allows creation of `ARef<T>` copy from `&Owned<T>`.
    
    Therefore, we split `AlwaysRefCounted` into `RefCounted` (which `ARef<T>`
    would require) and a marker trait to indicate that the type is always
    reference counted (and not `Ownable`) so the `&T` -> `ARef<T>` conversion
    is possible.
    
    - Rename `AlwaysRefCounted` to `RefCounted`.
    - Add a new unsafe trait `AlwaysRefCounted`.
    - Implement the new trait `AlwaysRefCounted` for the newly renamed
      `RefCounted` implementations. This leaves functionality of existing
      implementers of `AlwaysRefCounted` intact.
    
    Original patch by Oliver Mangold <oliver.mangold@pm.me> [1].
    
    Link: https://lore.kernel.org/r/20251117-unique-ref-v13-2-b5b243df1250@pm.me [1]
    Suggested-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Danilo Krummrich <dakr@kernel.org>
    Acked-by: Igor Korotin <igor.korotin.linux@gmail.com>
    Link: https://msgid.link/20260204-unique-ref-v14-2-17cb29ebacbb@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/device/property.rs
+++ rust/kernel/device/property.rs
@@ -14,7 +14,7 @@ use crate::{
     fmt,
     prelude::*,
     str::{CStr, CString},
-    sync::aref::ARef,
+    sync::aref::{ARef, RefCounted},
     types::Opaque,
 };
 
@@ -360,7 +360,7 @@ impl fmt::Debug for FwNodeReferenceArgs {
 }
 
 // SAFETY: Instances of `FwNode` are always reference-counted.
-unsafe impl crate::sync::aref::AlwaysRefCounted for FwNode {
+unsafe impl RefCounted for FwNode {
     fn inc_ref(&self) {
         // SAFETY: The existence of a shared reference guarantees that the
         // refcount is non-zero.
@@ -373,6 +373,10 @@ unsafe impl crate::sync::aref::AlwaysRefCounted for FwNode {
         unsafe { bindings::fwnode_handle_put(obj.cast().as_ptr()) }
     }
 }
+
+// SAFETY: We do not implement `Ownable`, thus it is okay to obtain an `ARef<FwNode>` from a
+// `&FwNode`.
+unsafe impl AlwaysRefCounted for FwNode {}
 
 enum Node<'a> {
     Borrowed(&'a FwNode),
