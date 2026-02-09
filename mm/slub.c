31a3b5b01dbb88085ef9a5e556d66cacaac521eb
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-09 15:38:15 +0100

    rust: mm: sheaf: allow use of C initialized static caches
    Extend the sheaf abstraction to support caches initialized by C at kernel
    boot time, in addition to dynamically created Rust caches.
    
    Introduce `KMemCache<T>` as a transparent wrapper around `kmem_cache` for
    static caches with `'static` lifetime. Rename the previous `KMemCache<T>`
    to `KMemCacheHandle<T>` to represent dynamically created, reference-counted
    caches.
    
    Add `Static` and `Dynamic` marker types along with `StaticSheaf` and
    `DynamicSheaf` type aliases to distinguish sheaves from each cache type.
    The `Sheaf` type now carries lifetime and allocation mode type parameters.
    
    Add `SBox::into_ptr()` and `SBox::static_from_ptr()` methods for passing
    allocations through C code via raw pointers.
    
    Add `KMemCache::from_raw()` for wrapping C-initialized static caches and
    `Sheaf::refill()` for replenishing a sheaf to a minimum size.
    
    Export `kmem_cache_prefill_sheaf`, `kmem_cache_return_sheaf`,
    `kmem_cache_refill_sheaf`, and `kmem_cache_alloc_from_sheaf_noprof` to
    allow Rust module code to use the sheaf API.
    
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Liam R. Howlett <Liam.Howlett@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: linux-mm@kvack.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -5009,6 +5009,7 @@ kmem_cache_prefill_sheaf(struct kmem_cache *s, gfp_t gfp, unsigned int size)
 
 	return sheaf;
 }
+EXPORT_SYMBOL(kmem_cache_prefill_sheaf);
 
 /*
  * Use this to return a sheaf obtained by kmem_cache_prefill_sheaf()
@@ -5064,6 +5065,7 @@ void kmem_cache_return_sheaf(struct kmem_cache *s, gfp_t gfp,
 	barn_put_full_sheaf(barn, sheaf);
 	stat(s, BARN_PUT);
 }
+EXPORT_SYMBOL(kmem_cache_return_sheaf);
 
 /*
  * refill a sheaf previously returned by kmem_cache_prefill_sheaf to at least
@@ -5117,6 +5119,7 @@ int kmem_cache_refill_sheaf(struct kmem_cache *s, gfp_t gfp,
 	*sheafp = sheaf;
 	return 0;
 }
+EXPORT_SYMBOL(kmem_cache_refill_sheaf);
 
 /*
  * Allocate from a sheaf obtained by kmem_cache_prefill_sheaf()
@@ -5154,6 +5157,7 @@ out:
 
 	return ret;
 }
+EXPORT_SYMBOL(kmem_cache_alloc_from_sheaf_noprof);
 
 unsigned int kmem_cache_sheaf_size(struct slab_sheaf *sheaf)
 {
