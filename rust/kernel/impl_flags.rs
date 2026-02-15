83fab9eb056ebfdbc7d0f7ef761c0ba6a9dec119
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-15 21:22:58 +0100

    LIST: [PATCH 2/2] rust: impl_flags: add bitwise operations with the underlying type
    Add bitwise or operations between the flag value enum and the underlying
    type. This is useful when manipulating flags from C API without round
    tripping into the Rust flag container type:
    
      let mut lim: bindings::queue_limits = unsafe { core::mem::zeroed() };
      ...
      if self.write_cache {
          lim.features |= request::Feature::WriteCache;
      }
    
    The above code would be needlessly verbose without this direct assignment
    option.
    
    Link: https://msgid.link/20260215-impl-flags-additions-v1-2-6538c8fb841c@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/impl_flags.rs
+++ rust/kernel/impl_flags.rs
@@ -257,6 +257,30 @@ macro_rules! impl_flags {
             }
         }
 
+        impl ::core::ops::BitXor<$flag> for $flags {
+            type Output = Self;
+            #[inline]
+            fn bitxor(self, rhs: $flag) -> Self::Output {
+                self ^ Self::from(rhs)
+            }
+        }
+
+        impl ::core::ops::BitOr<$flag> for $ty {
+            type Output = Self;
+
+            #[inline]
+            fn bitor(self, rhs: $flag) -> Self::Output {
+                self | <$flag as Into<Self>>::into(rhs)
+            }
+        }
+
+        impl ::core::ops::BitOrAssign<$flag> for $ty {
+            #[inline]
+            fn bitor_assign(&mut self, rhs: $flag) {
+                *self = *self | <$flag as Into<Self>>::into(rhs)
+            }
+        }
+
         impl $flags {
             /// Returns an empty instance where no flags are set.
             #[inline]
