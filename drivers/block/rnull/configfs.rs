996f0b9c30485b04daa9cd772f4fafb87c9fa7c7
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-15 10:21:30 +0100

    block: rnull: allow configuration of the maximum IO size
    Add module parameter and configfs option for controlling the maximum size
    of an IO for the emulated block device.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/configfs.rs
+++ drivers/block/rnull/configfs.rs
@@ -134,6 +134,7 @@ impl configfs::GroupOperations for Config {
                 zone_append_max_sectors: 26,
                 poll_queues: 27,
                 fua: 28,
+                max_sectors: 29,
             ],
         };
 
@@ -214,6 +215,7 @@ impl configfs::GroupOperations for Config {
                     requeue_inject,
                     #[cfg(CONFIG_BLK_DEV_RUST_NULL_FAULT_INJECTION)]
                     init_hctx_inject,
+                    max_sectors: 0,
                 }),
             }),
             default_groups,
@@ -296,6 +298,7 @@ struct DeviceConfigInner {
     requeue_inject: Arc<FaultConfig>,
     #[cfg(CONFIG_BLK_DEV_RUST_NULL_FAULT_INJECTION)]
     init_hctx_inject: Arc<FaultConfig>,
+    max_sectors: u32,
 }
 
 #[vtable]
@@ -353,6 +356,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 init_hctx_inject: guard.init_hctx_inject.clone(),
                 #[cfg(CONFIG_BLK_DEV_RUST_NULL_FAULT_INJECTION)]
                 timeout_inject: guard.timeout_inject.clone(),
+                max_sectors: guard.max_sectors,
             })?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -622,3 +626,4 @@ configfs_attribute! {
     },
 }
 configfs_simple_bool_field!(DeviceConfig, 28, fua);
+configfs_simple_field!(DeviceConfig, 29, max_sectors, u32);
